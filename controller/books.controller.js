09ddc585cf47d8f84346d72fdba8f0dbd8c1ae0d
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-06-08 10:07:23 +0200

    proof

--- controller/books.controller.js
+++ controller/books.controller.js
@@ -3,7 +3,7 @@ const booksModel = require("../model/books.model");
 async function getAllBooks(req, res) {
   const books = await booksModel.getAll();
   if (!books) {
-    res.status(404).json({ info: "Couldtn get books" });
+    res.status(404).json({ info: "Couldnt get books" });
     return;
   }
   res.status(200).json({ books: books });
